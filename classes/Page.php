9dfa7e20f51e732b74d2d08f230c8e5863da6790
Jason Shope <blurryphoto@gmail.com>
2024-08-20 20:29:11 -0400

    added ability to include template partials

--- classes/Page.php
+++ classes/Page.php
@@ -23,5 +23,62 @@ class Page {
   
   } // get_instance()
 
+  
+  
+  
+  
+  
+  function get_partial(string $file, ?string $suffix = null, $args = false)   {  
+    
+    
+    // Build the full path to the partial based 
+    // on what was passed.
+    $filename = ROOT_PATH . '/partials/' . $file;  
+    
+    
+    if ( !is_null($suffix) ):
+      
+      $filename .= '-' . $suffix;  
+    
+    endif;  
+    
+    
+    $filename .= '.php';  
+    
+    
+
+    // Include the specified partial file only if
+    // it is found.
+    if ( file_exists($filename) ):
+   
+      // Always make the Page class available.
+      $page = Page::get_instance();
+      
+   
+      // If we have args, extract them into variables
+      // for more readable code in the partial.
+      if ( is_array($args) && ! empty($args) ):
+        
+        extract($args);  
+      
+      endif;  
+      
+      
+      include $filename;
+      
+      
+    else:
+
+      return false;
+    
+    endif;  
+
+  
+  } // get_partial()
+
+  
+  
+  
+  
     
 } // ::Page
--- index.php
+++ index.php
@@ -8,11 +8,9 @@ $page = Page::get_instance();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Welcome</title>
-</head>
+
+<?php $page->get_partial('html-head'); ?>
+
 <body>
 
 <h1>Hello.</h1>
