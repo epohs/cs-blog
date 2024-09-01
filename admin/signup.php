6ba8a589a46f19dbbc00b1edb3f8c426099f4639
Jason Shope <blurryphoto@gmail.com>
2024-09-01 15:49:13 -0400

    Initial commit

---
+++ admin/signup.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php $page->get_partial('html-head', null, false, 'admin/partials'); ?>
+
+<body>
+  
+
+  
+
+<div class="page-wrap">
+
+  
+  <?php $page->get_partial('page-header', null, false, 'admin/partials'); ?>
+  
+
+  <main class="content">
+    
+    <?php $page->get_partial('errors', null, false, 'admin/partials'); ?>
+  
+    <h1>Signup</h1>
+  
+    <p><?= date("F j, Y, g:i a"); ?></p>
+    
+  </main>
+
+
+</div>
+
+
+
+
+
+</body>
+</html>
