6c1290b42f1d83fd71c7384e21a457c72de2e7d6
Jason Shope <blurryphoto@gmail.com>
2024-09-02 22:57:45 -0400

    Initial commit

---
+++ classes/Auth.php
@@ -0,0 +1,39 @@
+<?php
+
+class Auth {
+    
+    
+  private static $instance = null;  
+  
+
+  
+  
+  
+  private function __construct() {
+    
+
+    
+  } // __construct()
+  
+  
+  
+  
+  
+  
+  
+  
+  // Get the singleton instance of the class
+  public static function get_instance() {
+    
+    if ( is_null(self::$instance) ):
+      
+      self::$instance = new self();
+      
+    endif;
+    
+    return self::$instance;
+    
+  } // get_instance()
+
+    
+} // ::Auth
