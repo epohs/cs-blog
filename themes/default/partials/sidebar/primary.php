83ee9e11623796dee9757778a6b39e6c53d27efe
Jason Shope <blurryphoto@gmail.com>
2024-09-14 17:25:43 -0400

    added admin link

--- themes/default/partials/sidebar/primary.php
+++ themes/default/partials/sidebar/primary.php
@@ -3,7 +3,15 @@
   <nav class="nav">
     
     <ul class="nav-items">
+
       <li class="nav-item"><a href="<?php echo $page->url_for('/') ?>" class="nav-link">Home</a></li>
+
+      <?php if ( $page->is_admin() ): ?>
+
+        <li class="nav-item"><a href="<?php echo $page->url_for('admin/dash') ?>" class="nav-link">Admin</a></li>
+        
+      <?php endif; ?>
+
     </ul>
 
   </nav>
