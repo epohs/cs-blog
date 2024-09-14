11b3935a3b0f9ad8df5ff8b04c1a9594c76f3e69
epohs <blurryphoto@gmail.com>
2024-09-14 15:20:55 -0400

    log in/out links

--- themes/default/partials/page-header.php
+++ themes/default/partials/page-header.php
@@ -2,6 +2,14 @@
   
   page header<br>
   
-  User: <?php echo Session::get_key('user_selector'); ?><br>
+  <?php if ( $page->is_logged_in() ): ?>
+
+    <a href="<?php echo $page->url_for('logout') ?>">logout</a> (<?php echo Session::get_key('user_selector'); ?>)<br>
+
+  <?php else: ?>
+
+    <a href="<?php echo $page->url_for('login') ?>">login</a><br>
+
+  <?php endif; ?>
   
 </header>
