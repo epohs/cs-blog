3322e8ae341ff64a35c9fa98c10749281b07a3e7
Jason Shope <blurryphoto@gmail.com>
2024-09-16 14:06:26 -0400

    page structure

--- themes/default/profile.php
+++ themes/default/profile.php
@@ -5,12 +5,47 @@
 
 <body>
   
-<?php $page->get_partial('errors'); ?>
-  
-  
-<h1>Non-admin profile page.</h1>
 
-<p>RAW Session: <?= var_export($_SESSION, true); ?></p>
+  
+<?php $page->get_partial('page-header'); ?>
+  
+
+<div class="page-wrap">
+
+  <div class="page-body">
+
+
+    <?php $page->get_partial('sidebar/primary'); ?>
+
+
+    <main class="content">
+      
+      <?php $page->get_partial('errors'); ?>
+    
+      <h1>Non-admin profile page.</h1>
+
+      <p>Logged in as <strong><?php echo $cur_user['email']; ?></strong> (<?php echo $cur_user['selector']; ?>)</p>
+    
+      <p><?= Utils::format_date(); ?></p>
+      
+    </main> <!-- .content -->
+
+
+    <?php $page->get_partial('sidebar/secondary'); ?>
+    
+
+  </div> <!-- .page-body -->
+
+
+</div> <!-- .page-wrap -->
+
+
+<?php $page->get_partial('page-footer'); ?>
+
+
 
 </body>
 </html>
+
+  
+
