c4423afb9b743358b9894becfe2662e8b04faa9c
Jason Shope <blurryphoto@gmail.com>
2024-09-16 21:32:20 -0400

    debug session

--- themes/default/profile.php
+++ themes/default/profile.php
@@ -25,6 +25,8 @@
       <h1>Non-admin profile page.</h1>
 
       <p>Logged in as <strong><?php echo $cur_user['email']; ?></strong> (<?php echo $cur_user['selector']; ?>)</p>
+
+      <details><summary>Session:</summary> <?php echo var_export($_SESSION, true); ?></details>
     
       <p><?= Utils::format_date(); ?></p>
       
