e4bba8d5e2d28a9464df42b6e3f5dad373bb654f
Jason Shope <blurryphoto@gmail.com>
2024-11-17 15:11:52 -0500

    Initial commit

---
+++ themes/default/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Optional theme-specific functions file.
+ *
+ * If present, this file allows theme developers to define custom 
+ * PHP functions and logic specific to their theme. It is loaded 
+ * early in the initialization process to ensure customizations 
+ * are available throughout the application.
+ */
