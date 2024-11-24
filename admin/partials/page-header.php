aca7ebb45345ee4ea19782d4fca7e7bce82da381
Jason Shope <blurryphoto@gmail.com>
2024-11-24 13:54:26 -0500

    Add conditional link to /verify/ for unverified users

--- admin/partials/page-header.php
+++ admin/partials/page-header.php
@@ -3,6 +3,10 @@
   
   <?php if ( $page->is_logged_in() ): ?>
 
+    <?php if ( !$User->is_verified() ): ?>
+      <a href="<?php echo $page->url_for('verify') ?>">Verify</a><br>
+    <?php endif; ?>
+
     <a href="<?php echo $page->url_for('logout') ?>">logout</a> (<?php echo Session::get_key(['user', 'selector']); ?>)<br>
     <a href="<?php echo $page->url_for('profile') ?>">profile</a>
 
