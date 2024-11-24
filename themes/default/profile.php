ac9a79872355aa37fd1ffeba1687416425179719
Jason Shope <blurryphoto@gmail.com>
2024-11-24 15:59:00 -0500

    Add link to verify on profile page

--- themes/default/profile.php
+++ themes/default/profile.php
@@ -32,6 +32,10 @@
       
       <p>Your IP: <?php echo Utils::get_client_ip(); ?></p>
 
+      <?php if ( !$User->is_verified() ): ?>
+        <p>You need to <a href="<?php echo $page->url_for('profile'); ?>">verify your email address</a>.</p>
+      <?php endif; ?>
+
       <details><summary>Session:</summary> <?php echo var_export($_SESSION, true); ?></details>
     
       <p><?= Utils::format_date(); ?></p>
