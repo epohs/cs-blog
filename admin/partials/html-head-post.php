a48b72339fbdff36d4e9970e58e9d48616ba3a9b
Jason Shope <blurryphoto@gmail.com>
2025-02-23 16:46:49 -0500

    Update Trix

--- admin/partials/html-head-post.php
+++ admin/partials/html-head-post.php
@@ -4,8 +4,8 @@
 <title><?php echo $Page->get_page_title(); ?></title>
 <link rel="stylesheet" href="<?php echo $Page->site_root(); ?>/css/main.css" type="text/css">
 
-<link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.8/dist/trix.css">
-<script type="text/javascript" src="https://unpkg.com/trix@2.0.8/dist/trix.umd.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.1.12/dist/trix.css">
+<script type="text/javascript" src="https://unpkg.com/trix@2.1.12/dist/trix.umd.min.js"></script>
 <?php /* <script src="https://unpkg.com/turndown/dist/turndown.js"></script> */ ?>
 
 
