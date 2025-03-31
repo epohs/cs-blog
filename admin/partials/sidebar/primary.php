465adce2ea5ba0eba9f1008108d2b14b5285571d
Jason Shope <blurryphoto@gmail.com>
2025-03-30 22:46:58 -0400

    Ensure admin-only sidebar items are not shown to non-admin users

--- admin/partials/sidebar/primary.php
+++ admin/partials/sidebar/primary.php
@@ -3,10 +3,19 @@
   <nav class="nav">
     
     <ul class="nav-items">
-      <li class="nav-item"><a href="<?php echo $Page->url_for('admin/post/new') ?>" class="nav-link">New post</a></li>
+      
       <li class="nav-item"><a href="<?php echo $Page->url_for('/') ?>" class="nav-link">Home</a></li>
-      <li class="nav-item"><a href="<?php echo $Page->url_for('admin/post/list') ?>" class="nav-link">All Posts</a></li>
-      <li class="nav-item"><a href="<?php echo $Page->url_for('admin/user/list') ?>" class="nav-link">All Users</a></li>
+      
+      <?php if ( $User->is_admin() ): ?>
+  
+        <li class="nav-item"><a href="<?php echo $Page->url_for('admin/dash') ?>" class="nav-link">Dashboard</a></li>
+        
+        <li class="nav-item"><a href="<?php echo $Page->url_for('admin/post/new') ?>" class="nav-link">New post</a></li>
+        <li class="nav-item"><a href="<?php echo $Page->url_for('admin/post/list') ?>" class="nav-link">All Posts</a></li>
+        <li class="nav-item"><a href="<?php echo $Page->url_for('admin/user/list') ?>" class="nav-link">All Users</a></li>
+      
+      <?php endif; ?>
+      
     </ul>
 
   </nav>
