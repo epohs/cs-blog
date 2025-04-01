3f7cc9ccb0662224b119ef1db0f1925f45065315
Jason Shope <blurryphoto@gmail.com>
2025-03-31 22:13:27 -0400

    Use get_display_name() for logged in indicator

--- admin/partials/page-header.php
+++ admin/partials/page-header.php
@@ -7,7 +7,7 @@
       <a href="<?php echo $Page->url_for('verify') ?>">Verify</a><br>
     <?php endif; ?>
 
-    <a href="<?php echo $Page->url_for('logout') ?>">logout</a> (<?php echo Session::get_key(['user', 'selector']); ?>)<br>
+    <a href="<?php echo $Page->url_for('logout') ?>">logout</a> (<?php echo $User->get_display_name(Session::get_key(['user', 'selector'])); ?>)<br>
     <a href="<?php echo $Page->url_for('profile') ?>">profile</a>
 
   <?php else: ?>
